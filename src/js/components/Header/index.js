52c60b1cbbca0790a13688e4895ab30144b8f554
Javier <jarai.antonio@gmail.com>
2018-09-03 17:32:30 +0200

    first commit

--- src/js/components/Header/index.js
+++ src/js/components/Header/index.js
@@ -7,7 +7,7 @@ class Header extends Component {
     render() {
         return (
             <header>
-                <h1 className="title">Simple React Template</h1>
+                <h1 className="title">FramexFrame</h1>
             </header>
         )
     }
